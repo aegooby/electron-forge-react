9882e6c101608a6d08e085745d9113adb400d42e
aegooby <aegooby@gmail.com>
2021-02-09 06:28:29 +0300

    TypeScript

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,20 +1,17 @@
-const rules = require("./webpack.rules");
+const rules = require('./webpack.rules');
+const plugins = require('./webpack.plugins');
 
-const cssRules =
-{
-    test: /\.css$/,
-    use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+rules.push({
+  test: /\.css$/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+});
+
+module.exports = {
+  module: {
+    rules,
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
+  },
 };
-
-rules.push(cssRules);
-
-const jsxRules =
-{
-    test: /\.jsx?$/,
-    exclude: /node_modules/,
-    use: [{ loader: "babel-loader" }]
-};
-
-rules.push(jsxRules);
-
-module.exports = { module: { rules, }, };
--- webpack.rules.js
+++ webpack.rules.js
@@ -1,16 +1,27 @@
-module.exports =
-[
-    {
-        test: /\.node$/,
-        use: "node-loader",
+module.exports = [
+  // Add support for native node modules
+  {
+    test: /\.node$/,
+    use: 'node-loader',
+  },
+  {
+    test: /\.(m?js|node)$/,
+    parser: { amd: false },
+    use: {
+      loader: '@marshallofsound/webpack-asset-relocator-loader',
+      options: {
+        outputAssetBase: 'native_modules',
+      },
     },
-    {
-        test: /\.(m?js|node)$/,
-        parser: { amd: false },
-        use:
-        {
-            loader: "@marshallofsound/webpack-asset-relocator-loader",
-            options: { outputAssetBase: "native_modules", },
-        },
-    },
+  },
+  {
+    test: /\.tsx?$/,
+    exclude: /(node_modules|\.webpack)/,
+    use: {
+      loader: 'ts-loader',
+      options: {
+        transpileOnly: true
+      }
+    }
+  },
 ];
